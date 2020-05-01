cd9447c24bc5689396b5f6da32a3d89423d369d0
JeffMorhous <jobboy18@gmail.com>
2020-05-01 13:55:06 -0400

    Initial Commit

---
+++ What Are The Odds/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  What Are The Odds
+//
+//  Created by Jeff Morhous on 5/1/20.
+//  Copyright © 2020 Jeff Morhous. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
