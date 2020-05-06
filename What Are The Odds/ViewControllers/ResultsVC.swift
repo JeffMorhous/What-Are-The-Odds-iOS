42226f002dbc7c11b3a3763ebbf5af12283d2048
JeffMorhous <jobboy18@gmail.com>
2020-05-06 09:36:48 -0400

    Show game results

--- What Are The Odds/ViewControllers/ResultsVC.swift
+++ What Are The Odds/ViewControllers/ResultsVC.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ResultsVC: UIViewController {
 
+    // UI Elements
+    let titleLabel = UILabel()
+    let userNumberLabel = UILabel()
+    let computerNumberLabel = UILabel()
+    let horizontalStack = UIStackView()
+    let resultLabel = UILabel()
+    
+    // Class Properties
     var selectedOdds: Int!
     
     override func viewDidLoad() {
@@ -19,17 +27,77 @@ class ResultsVC: UIViewController {
         view.backgroundColor = .systemBackground
         self.title = "What Are The Odds?"
         navigationController?.isNavigationBarHidden = true
-        // TODO: Show a  label with "1 in \(odds)"
-        // TODO: Show two numbers generated
-        // TODO: Add same odds and half odds buttons
+        // Show a  label with "1 in \(odds)"
+        configureTitleLabel()
         playGame()
+        // TODO: Add same odds and half odds buttons
     }
     
-    // Mark: Game Logic
+    // MARK: UI Configuration
+    private func configureTitleLabel() {
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        titleLabel.text = "1 in \(selectedOdds!)"
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            titleLabel.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    private func configureResultNumbers(userNumber: Int, computerNumber: Int) {
+        view.addSubview(horizontalStack)
+        horizontalStack.translatesAutoresizingMaskIntoConstraints = false
+        horizontalStack.axis = .horizontal
+        horizontalStack.alignment = .center // .Leading .FirstBaseline .Center .Trailing .LastBaseline
+        horizontalStack.distribution = .equalCentering // .FillEqually .FillProportionally .EqualSpacing .EqualCentering
+        
+        NSLayoutConstraint.activate([
+           horizontalStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 100),
+           horizontalStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -100),
+           horizontalStack.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+           horizontalStack.heightAnchor.constraint(equalToConstant: 200)
+        ])
+        
+        horizontalStack.addArrangedSubview(userNumberLabel)
+        userNumberLabel.text = "\(userNumber)"
+        userNumberLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        
+        horizontalStack.addArrangedSubview(computerNumberLabel)
+        computerNumberLabel.text = "\(computerNumber)"
+        computerNumberLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+    }
+    
+    // Inform the player of the game results
+    private func showGameResults(sameOdds: Bool){
+        if(sameOdds){
+            resultLabel.text = "The numbers are the same, you lose!"
+        } else {
+            resultLabel.text = "The numbers are different, and you're safe!"
+        }
+        view.addSubview(resultLabel)
+        resultLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            resultLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            resultLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            resultLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            resultLabel.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    // MARK: Game Logic
     
     private func playGame(){
         let firstNumber = generateNumber(range: selectedOdds)
         let secondNumber = generateNumber(range: selectedOdds)
+        
+        configureResultNumbers(userNumber: firstNumber, computerNumber: secondNumber)
         
         var sameOdds = false
         if(firstNumber == secondNumber){
@@ -40,16 +108,7 @@ class ResultsVC: UIViewController {
     
     // Generate a number between 1 and the passed parameter, range
     private func generateNumber(range: Int) -> Int {
-        // TODO: Implement
-    }
-    
-    // Inform the player of the game results
-    private func showGameResults(sameOdds: Bool){
-        if(sameOdds){
-            // TODO: Show the player that they've lost the game
-        } else {
-            // TODO: Show the player taht they've won the game
-        }
+        return Int.random(in: 1...range)
     }
 
 }
